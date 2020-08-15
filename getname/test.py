bb449f198ff512468014af3989634f2021a398a8
chi_muu <chi_muu@debianbare.debianbare.lan>
2020-08-15 06:29:14 -0700

    fix

--- getname/test.py
+++ getname/test.py
@@ -3,7 +3,7 @@ import json
 
 def get_config():
     api = "http://v2caddy:2019/load"
-    res = requests.get(api, headers={'Content-Type': 'text/json'})
+    res = requests.get(api)
     js = json.loads(res.text)
     print(js)
 
